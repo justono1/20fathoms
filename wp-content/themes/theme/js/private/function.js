1c0958f9d9a4347051d2688b49e6faf2826eb320
Justin Gauthier <itsme@justingauthier.com>
2018-04-30 22:12:36 -0400

    Mobile Updates and mobile nav

--- wp-content/themes/theme/js/private/function.js
+++ wp-content/themes/theme/js/private/function.js
@@ -23,6 +23,10 @@ $(document).ready(function() {
 		$('.main-navigation').toggleClass('is-hidden-mobile-tablet');
 	});
 
+	$('.mobile-nav-trigger').on('click', function() {
+		$(".main-menu").fadeToggle();
+	});
+
 	$('.nav-main-item>a').on('click', function() {
 		var w = $(window).width();
 
